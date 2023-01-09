c44920cf3f5fdf620c57dc2f08c33d76c17ff85b
haftamudesta <desta451616@hotmail.com>
2023-01-09 22:52:40 +0300

    Modify script.js

--- script.js
+++ script.js
@@ -1,40 +1,42 @@
 function addItem() {
-    let library = {
-        title: document.getElementById('title').value,
-        author: document.getElementById('author').value,
-    };
+  let library = {
+    title: document.getElementById("title").value,
+    author: document.getElementById("author").value,
+  };
 
-    let bookList = document.getElementById('book-list');
+  let bookList = document.getElementById("book-list");
 
-    let newBook = document.createElement('p');
-    newBook.innerHTML= `${library.title}<br>${library.author}`;
+  let newBook = document.createElement("p");
+  newBook.innerHTML = `${library.title}<br>${library.author}`;
 
-    let removeBook = document.createElement('button');
-    removeBook.id = 'remove-btn';
-    removeBook.classList.add('${library.title}');
-    removeBook.innerHTML = 'Remove';
+  let removeBook = document.createElement("button");
+  removeBook.id = "remove-btn";
+  removeBook.classList.add("${library.title}");
+  removeBook.innerHTML = "Remove";
 
-    let rule = document.createElement('hr');
+  let rule = document.createElement("hr");
 
-    bookList.appendChild(newBook);
-    bookList.appendChild(removeBook);
-    bookList.appendChild(rule);
+  bookList.appendChild(newBook);
+  bookList.appendChild(removeBook);
+  bookList.appendChild(rule);
+}
+
+function removeItem() {
+  // get the list of items
+  let bookList = document.getElementById("book-list");
+  let newBook = document.querySelector("p");
+  // remove the last item from the list
+  if (newBook) {
+    newBook.parentNode.removeChild(newBook);
   }
-  
-  function removeItem() {
-    // get the list of items
-    let bookList = document.getElementById('book-list');
-    let newBook = document.querySelector('p');
-    // remove the last item from the list
-    if (newBook) {
-        newBook.parentNode.removeChild(newBook);
-    }
-  }
-  
-  // add event listeners to the buttons
-  let addBook = document.getElementById('add-btn');
-  addBook.addEventListener('click', addItem);
-  
-  
+}
+
+// add event listeners to the buttons
+let addBook = document.getElementById("add-btn");
+addBook.addEventListener("click", function () {
+  addItem();
+  document.getElementById("forms").reset();
+});
+
 //   document.getElementById('remove-btn').addEventListener('click', removeItem);
-  removeBook.addEventListener('click', removeItem);
+removeBook.addEventListener("click", removeItem);
